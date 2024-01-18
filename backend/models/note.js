deacab0e6628711ee7ecf33506221dff3ec5c2c4
FelixDot <felix.tran002@gmail.com>
2024-01-18 13:45:47 +0100

    uibuild

--- backend/models/note.js
+++ backend/models/note.js
@@ -1,35 +1,30 @@
-const mongoose = require("mongoose");
+/**
+ * Only defines the Mongoose schema for notes
+ */
 
-mongoose.set("strictQuery", false);
+const mongoose = require('mongoose')
 
-const url = process.env.MONGODB_URI;
-
-console.log("connecting to", url);
-
-mongoose
-  .connect(url)
-  .then((result) => {
-    console.log("connected to MongoDB");
-  })
-  .catch((error) => {
-    console.log("error connecting to MongoDB:", error.message);
-  });
+mongoose.set('strictQuery', false)
 
 const noteSchema = new mongoose.Schema({
-  content: {
-    type: String,
-    minLength: 5,
-    required: true,
-  },
-  important: Boolean,
-});
+    content: {
+        type: String,
+        required: true,
+        minlength: 5
+    },
+    important: Boolean,
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }
+})
 
-noteSchema.set("toJSON", {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
-  },
-});
+noteSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    },
+})
 
-module.exports = mongoose.model("Note", noteSchema);
+module.exports = mongoose.model('Note', noteSchema)
